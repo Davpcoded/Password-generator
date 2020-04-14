4a28ac7e44a40787bbc9f48a84dc4e0fc99b979e
Daniel Villarroel <handonsk82894@gmail.com>
2020-04-13 23:07:15 -0700

    created function that generates random with upper and lower case

--- script.js
+++ script.js
@@ -16,7 +16,8 @@ function getRandomSymbol() {
     return symbols[Math.floor(Math.random() * symbols.length)];
   }
 
-
+//Created variable to store the password lenght
+var passwordLength = [8, 128];
 
 //Created an array to store password criteria
 var passwordCrit = ["numerical", "alphanumerical", "with symbols"];
@@ -27,9 +28,10 @@ var selectPass = prompt("Select password criteria 'numerical', 'alphanumerical',
 
     //Created If/else statement to determine which criteria has been selected by the user
 if (selectPass === passwordCrit[0]) {
-    var passwordLength = prompt("Now select password lenght min 8 charac. Max 128 charac.");
-        
+    var numericalPassword = prompt("Now select password lenght min 8 charac. Max 128 charac.");
+    passwordLength = numericalPassword.valueOf    
 }
+
 else if (selectPass === passwordCrit[1]) {
     prompt("Now select password lenght min 8 charac. Max 128 charac.");
 }
@@ -40,5 +42,5 @@ else  {
     alert("Please select a valid input");
 }
 
-console.log(passwordCrit[1]);
+console.log(numericalPassword);
 
